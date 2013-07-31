93dcce419f6fd188f46c1670cb0c8780920feb22
kiptoo <evantoo56@gmail.com>
2013-07-31 14:11:19 +0300

    Initializing project

---
+++ Documents/Kenya EMR/SpringMVCApp/src/main/java/com/springapp/mvc/syncmodel/getall.java
@@ -0,0 +1,11 @@
+package com.springapp.mvc.syncmodel;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: kip
+ * Date: 7/29/13
+ * Time: 3:48 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class getall {
+}
